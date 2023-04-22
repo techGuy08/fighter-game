7a3af14fe96adcfd2384caa8f36ac4b4489bc06e
Amine S <42660934+techGuy08@users.noreply.github.com>
2023-04-22 05:55:29 +0000

    initial commit

---
+++ js/classes.js
@@ -0,0 +1,169 @@
+const canvas = document.querySelector("#game");
+const c = canvas.getContext("2d");
+
+class Sprite {
+  constructor({ position, imgSrc, scale, framesMax }) {
+    this.position = position;
+    this.height = 250;
+    this.width = 50;
+    this.image = new Image();
+    this.image.src = imgSrc;
+    this.scale = scale;
+    this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesCount = 0;
+    this.holdFrames = 5;
+  }
+
+  draw() {
+    // c.fillStyle = "red";
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    // c.fillStyle = "red";
+    // c.fillRect(
+    //   this.position.x - this.attackBox.offset.x,
+    //   this.position.y,
+    //   this.attackBox.width,
+    //   this.attackBox.height
+    // );
+    c.drawImage(
+      this.image,
+      this.framesCurrent * (this.image.width / this.framesMax),
+      0,
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      (this.image.width / this.framesMax) * this.scale,
+      this.image.height * this.scale
+    );
+  }
+  updateFrames() {
+    this.framesCount++;
+    if (this.framesCount % this.holdFrames === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
+        if (this.image.src != this.sprites.death.image.src) {
+          this.framesCurrent = 0;
+        }
+      }
+    }
+  }
+  update() {
+    this.draw();
+    this.updateFrames();
+  }
+}
+
+class Fighter extends Sprite {
+  constructor({
+    position,
+    velocity,
+    controlsKeys,
+    color = "red",
+    offset,
+    healthBarSelector,
+    imgSrc,
+    scale,
+    framesMax,
+    sprites,
+  }) {
+    super({ position, imgSrc, scale, framesMax });
+    let keys = {};
+    controlsKeys.forEach(function (value) {
+      keys[value] = {
+        pressed: false,
+      };
+    });
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+    this.width = 50;
+    this.lastKey;
+    this.keys = keys;
+    this.color = color;
+    this.isGrounded = false;
+    this.isAttacking = false;
+    this.isAttacked = false;
+    this.isTakingHit = false;
+    this.hp = 100;
+    this.healthBarEl = document.querySelector(healthBarSelector);
+    this.speed = 10;
+    this.attackBox = {
+      position: this.position,
+      width: 200,
+      height: this.width,
+      offset,
+    };
+    this.sprites = sprites;
+    for (const sprite in this.sprites) {
+      this.sprites[sprite].image = new Image();
+      this.sprites[sprite].image.src = this.sprites[sprite].imgSrc;
+
+      this.sprites[sprite].audio = new Audio(this.sprites[sprite].audioSrc);
+    }
+  }
+
+  update() {
+    this.draw();
+    this.updateFrames();
+
+    if (this.velocity.y > 0) {
+      this.velocity.y += gravity;
+    }
+    if (this.hp > 0) {
+      this.position.y += this.velocity.y;
+
+      this.position.x += this.velocity.x;
+    }
+
+    if (
+      this.position.y + this.height + this.velocity.y >=
+      canvas.height - 200
+    ) {
+      this.velocity.y = 0;
+      this.isGrounded = true;
+    } else this.velocity.y += gravity;
+
+    if (this.position.x + this.velocity.x <= -200) {
+      this.position.x = -200;
+    } else if (this.position.x + this.width >= canvas.width - 300) {
+      this.position.x = canvas.width - this.width - 300;
+    }
+    this.healthBarEl.querySelector(".health-line").style.width = this.hp + "%";
+  }
+  attack() {
+    if (!this.isAttacking && this.hp > 0) {
+      this.isAttacking = true;
+      setTimeout(() => {
+        this.isAttacking = false;
+        this.isAttacked = false;
+      }, 600);
+    }
+  }
+  damageTaken(damage) {
+    if (isGameRunning) {
+      if (this.hp >= damage) {
+        this.hp -= damage;
+        this.isTakingHit = true;
+        setTimeout(() => {
+          this.isTakingHit = false;
+        }, 600);
+      } else if (this.hp < damage) {
+        this.hp = 0;
+      }
+    }
+    if (this.hp == 0) {
+      isGameRunning = false;
+      decideGameEnd(player, player2, timerId);
+    }
+  }
+  switchSprite(name) {
+    if (this.image === this.sprites.run.image && name === "attack1") return;
+
+    if (this.sprites[name]) {
+      this.image = this.sprites[name].image;
+      this.framesMax = this.sprites[name].framesMax;
+    }
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,323 @@
+canvas.width = 1024;
+canvas.height = 576;
+
+const gravity = 0.16;
+let isGameRunning = true;
+
+const player = new Fighter({
+  position: {
+    x: 0,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  },
+  controlsKeys: ["a", "s", "d", "w"],
+  offset: {
+    x: 0,
+    y: 0,
+  },
+  healthBarSelector: ".player1 .health-bar",
+  imgSrc: "./img/akai/idle.png",
+  scale: 4,
+  framesMax: 10,
+  sprites: {
+    idle: {
+      imgSrc: "./img/akai/idle.png",
+      framesMax: 10,
+    },
+    run: {
+      imgSrc: "./img/akai/run.png",
+      framesMax: 8,
+    },
+    jump: {
+      imgSrc: "./img/akai/Going-Up.png",
+      framesMax: 3,
+    },
+    fall: {
+      imgSrc: "./img/akai/Going-Down.png",
+      framesMax: 3,
+    },
+    attack1: {
+      imgSrc: "./img/akai/attack1.png",
+      framesMax: 7,
+      audioSrc: "./img/attack.wav",
+    },
+    death: {
+      imgSrc: "./img/akai/death.png",
+      framesMax: 11,
+    },
+    hit: {
+      imgSrc: "./img/akai/Take-Hit.png",
+      framesMax: 3,
+    },
+  },
+});
+
+const player2 = new Fighter({
+  position: {
+    x: canvas.width - 500,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  },
+  controlsKeys: ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"],
+  offset: {
+    x: 145,
+    y: 0,
+  },
+  healthBarSelector: ".player2 .health-bar",
+  imgSrc: "./img/hanzo/idle.png",
+  scale: 4,
+  framesMax: 11,
+  sprites: {
+    idle: {
+      imgSrc: "./img/hanzo/idle.png",
+      framesMax: 11,
+    },
+    run: {
+      imgSrc: "./img/hanzo/run.png",
+      framesMax: 8,
+    },
+    jump: {
+      imgSrc: "./img/hanzo/jump.png",
+      framesMax: 4,
+    },
+    fall: {
+      imgSrc: "./img/hanzo/fall.png",
+      framesMax: 4,
+    },
+    attack1: {
+      imgSrc: "./img/hanzo/attack.png",
+      framesMax: 6,
+    },
+    death: {
+      imgSrc: "./img/hanzo/death.png",
+      framesMax: 9,
+    },
+    hit: {
+      imgSrc: "./img/hanzo/Take-Hit.png",
+      framesMax: 4,
+    },
+  },
+});
+
+function update() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (player.keys.w.pressed && player.isGrounded) {
+    player.velocity.y = -10;
+    player.isGrounded = false;
+  }
+  if (player2.keys["ArrowUp"].pressed && player2.isGrounded) {
+    player2.velocity.y = -10;
+    player2.isGrounded = false;
+  }
+
+  if (player.isGrounded && !player.isTakingHit) {
+    player.switchSprite("idle");
+  } else if (player.velocity.y < 0) {
+    player.switchSprite("jump");
+  } else if (player.velocity.y > 0) {
+    player.switchSprite("fall");
+  }
+
+  if (player.keys.a.pressed && player.lastKey == "a") {
+    player.velocity.x = -player.speed;
+    if (player.isGrounded) player.switchSprite("run");
+  } else if (player.keys.d.pressed && player.lastKey == "d") {
+    player.velocity.x = player.speed;
+    if (player.isGrounded) player.switchSprite("run");
+  } else {
+    player.velocity.x = 0;
+  }
+
+  if (player.isTakingHit) {
+    player.velocity.x = -2;
+  }
+  if (player.isAttacking) {
+    player.switchSprite("attack1");
+  }
+  if (player.isTakingHit) {
+    player.switchSprite("hit");
+  }
+  if (player.hp == 0 && player.image !== player.sprites.death.image) {
+    player.switchSprite("death");
+  }
+
+  if (player2.isGrounded && !player2.isTakingHit) {
+    player2.switchSprite("idle");
+  } else if (player2.velocity.y < 0) {
+    player2.switchSprite("jump");
+  } else if (player2.velocity.y > 0) {
+    player2.switchSprite("fall");
+  }
+  if (player2.keys["ArrowLeft"].pressed && player2.lastKey == "ArrowLeft") {
+    player2.velocity.x = -player2.speed;
+    if (player2.isGrounded) player2.switchSprite("run");
+  } else if (
+    player2.keys["ArrowRight"].pressed &&
+    player2.lastKey == "ArrowRight"
+  ) {
+    player2.velocity.x = player2.speed;
+    if (player2.isGrounded) player2.switchSprite("run");
+  } else {
+    player2.velocity.x = 0;
+  }
+
+  if (player2.isAttacking) {
+    player2.switchSprite("attack1");
+  }
+  if (player2.isTakingHit) {
+    player2.switchSprite("hit");
+  }
+  if (player2.hp == 0 && player2.image !== player2.sprites.death.image) {
+    player2.switchSprite("death");
+  }
+
+  player.update();
+  player2.update();
+
+  if (
+    collisionDetect(player, player2) &&
+    player.isAttacking &&
+    !player2.isTakingHit &&
+    isGameRunning
+  ) {
+    let attackDamage = Math.floor(Math.random() * 20) + 1;
+    // player.isAttacked = true;
+    player2.damageTaken(attackDamage);
+    player2.isTakingHit = true;
+    console.log("player1 attacked");
+  }
+  if (
+    collisionDetect(player2, player) &&
+    player2.isAttacking &&
+    !player.isTakingHit &&
+    isGameRunning
+  ) {
+    let attackDamage = Math.floor(Math.random() * 20) + 1;
+    // player2.isAttacking = false;
+    player.damageTaken(attackDamage);
+    player.isTakingHit = true;
+    console.log("player2 attacked");
+  }
+  // console.log(collisionDetect(player2, player));
+  // console.log(collisionDetect(player, player2));
+}
+function collisionDetect(rect1, rect2) {
+  let x =
+    (rect1.position.x + rect1.attackBox.width - rect1.attackBox.offset.x >=
+      rect2.position.x &&
+      rect1.position.x + rect1.attackBox.width - rect1.attackBox.offset.x <=
+        rect2.position.x + rect2.width) ||
+    (rect1.position.x - rect1.attackBox.offset.x <= rect2.position.x &&
+      rect1.position.x + rect1.attackBox.width - rect1.attackBox.offset.x >=
+        rect2.position.x + rect2.width) ||
+    (rect1.position.x - rect1.attackBox.offset.x >= rect2.position.x &&
+      rect1.position.x - rect1.attackBox.offset.x <=
+        rect2.position.x + rect2.width);
+  let y =
+    (rect1.attackBox.position.y >= rect2.position.y &&
+      rect1.attackBox.position.y <= rect2.position.y + rect2.height) ||
+    (rect1.attackBox.position.y + rect1.attackBox.height >= rect2.position.y &&
+      rect1.attackBox.position.y + rect1.attackBox.height <=
+        rect2.position.y + rect2.height);
+  return x && y;
+}
+function animate() {
+  window.requestAnimationFrame(animate);
+  update();
+}
+
+animate();
+
+let timer = 60;
+let timerId;
+function runTimer() {
+  const timerEl = document.querySelector(".timerbox");
+  if (timer > 0) {
+    timerId = setTimeout(runTimer, 1000);
+    timer--;
+    if (timer < 10) {
+      timerEl.innerHTML = "0";
+    } else {
+      timerEl.innerHTML = "";
+    }
+    timerEl.innerHTML += timer;
+  } else {
+    decideGameEnd(player, player2, timerId);
+  }
+}
+runTimer();
+
+function decideGameEnd(player, player2, timerId) {
+  const gameOverEl = document.querySelector(".game-over");
+  const gameResultEl = gameOverEl.querySelector(".game-result");
+  const winnerNameEl = gameOverEl.querySelector(".game-winner");
+  let result = "Winner!";
+  if (player.hp > player2.hp) {
+    winnerNameEl.innerHTML = "Player 1";
+    player2.isTakingHit = false;
+  } else if (player2.hp > player.hp) {
+    winnerNameEl.innerHTML = "Player 2";
+    player.isTakingHit = false;
+  } else {
+    result = "Draw!";
+    winnerNameEl.innerHTML = "";
+  }
+  gameResultEl.innerHTML = result;
+  gameOverEl.classList.add("active");
+  isGameRunning = false;
+  clearTimeout(timerId);
+}
+
+const bgAudio = new Audio("./img/bg.mp3");
+bgAudio.loop = true;
+bgAudio.volume = 0.5;
+bgAudio.play();
+
+window.addEventListener("keydown", function (e) {
+  bgAudio.play();
+  switch (e.key) {
+    case "a":
+    case "w":
+    case "d":
+      player.keys[e.key].pressed = true;
+      if (e.key == "a" || e.key == "d") player.lastKey = e.key;
+      break;
+    case "s":
+      player.attack();
+      break;
+    case "ArrowLeft":
+    case "ArrowRight":
+    case "ArrowUp":
+      player2.keys[e.key].pressed = true;
+      if (e.key == "ArrowLeft" || e.key == "ArrowRight")
+        player2.lastKey = e.key;
+      break;
+    case "ArrowDown":
+      player2.attack();
+      break;
+  }
+});
+
+window.addEventListener("keyup", function (e) {
+  switch (e.key) {
+    case "a":
+    case "s":
+    case "d":
+    case "w":
+      player.keys[e.key].pressed = false;
+      break;
+    case "ArrowLeft":
+    case "ArrowRight":
+    case "ArrowDown":
+    case "ArrowUp":
+      player2.keys[e.key].pressed = false;
+      break;
+  }
+});
